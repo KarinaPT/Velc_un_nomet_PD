dd90ba6cc2b85084a23cb9df066dbb1cd4245410
Karina <kstifurska@gmail.com>
2021-04-23 11:58:14 +0300

    Skripti, sanas izmainas

---
+++ Assets/Skripti/Izvelne1Skripts.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+//Importe, lai varetu ieladet ainas
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Izvelne1Skripts : MonoBehaviour
+{
+    //Izdarot klikšķi uz pogas "uzAinu", tiks atvērta ains "izvele1"
+    public void uzIzvelni()
+    {
+        SceneManager.LoadScene("izvele1", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Skripti/UzGalvenoSkripts.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+//Importe, lai varetu ieladet ainas
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UzGalvenoSkripts : MonoBehaviour
+{
+    //Izdarot klikski uz pogas "uzIzvelni", tiks atverta ainna "galvenaAina"
+    public void uzGalveno()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+   }
